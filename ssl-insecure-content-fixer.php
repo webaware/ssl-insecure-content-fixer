adaefb93e386fe91d73e65db0c0b4ad2d09449b9
Ross McKay <rmckay@webaware.com.au>
2014-04-18 03:01:52 +0000

    bump wp compat 3.9; readme fixes; license text update

--- ssl-insecure-content-fixer.php
+++ ssl-insecure-content-fixer.php
@@ -11,9 +11,10 @@ Author URI: http://www.webaware.com.au/
 /*
 copyright (c) 2012-2014 WebAware Pty Ltd (email : rmckay@webaware.com.au)
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License, version 2, as
-published by the Free Software Foundation.
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -22,7 +23,7 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 if (!defined('SSLFIX_PLUGIN_ROOT')) {

2febf6ce63cc9e12d90f9986d54bc75c993eccdf
Ross McKay <rmckay@webaware.com.au>
2018-02-05 08:54:07 +1100

    update warning notice views

--- views/requires-pcre.php
+++ views/requires-pcre.php
@@ -1,5 +1,10 @@
+<?php
+if (!defined('ABSPATH')) {
+	exit;
+}
+?>
 
-<div class="error">
+<div class="notice notice-error">
 	<p><?php printf(__('SSL Insecure Content Fixer requires <a target="_blank" rel="noopener" href="%1$s">PCRE</a> version %2$s or higher; your website has PCRE version %3$s', 'ssl-insecure-content-fixer'),
-		'http://php.net/manual/en/book.pcre.php', esc_html($pcre_min), esc_html(PCRE_VERSION)); ?></p>
+		'https://secure.php.net/manual/en/book.pcre.php', esc_html($pcre_min), esc_html(PCRE_VERSION)); ?></p>
 </div>
